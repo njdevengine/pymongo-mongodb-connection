40f2ea7cdaaf0ad9bc9cda76ba248275545b6622
njdevengine <newjerseydevelopment@gmail.com>
2019-04-02 21:11:50 -0400

    Update pymongo.py

--- pymongo.py
+++ pymongo.py
@@ -4,3 +4,10 @@ import pymongo
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 print(client)
+
+# make a database called classDB
+db = client.classDB
+
+db.classroom.insert_one({"Name":"Seth",
+                        "Row":3,
+                        "Favorite Code":"bins"})
